0df70193a6209a305ba06fc7e72287835b05eb81
Maria Paula Mejía <mapis4045@gmail.com>
2025-04-04 15:29:02 -0500

    Solución ejercicios de práctica

---
+++ tallerCasa.js
@@ -0,0 +1,62 @@
+// Actividad 1
+
+// (Agregar un Elemento al Inicio): Crea un array con algunos elementos,
+// como nombres de animales. Utiliza el método unshift() para agregar un nuevo
+// elemento al inicio del array. Luego, imprima el array actualizado.
+
+// let AnimalNames = ["Perro", "Gato","Conejo"]
+
+// console.log("Nombres de animales en la lista incial: " + AnimalNames)
+
+// AnimalNames.unshift("Tortuga")
+
+// console.log("Nombres de animales en la lista final: " + AnimalNames)
+
+// Actividad 2 (Eliminar el Primer Elemento): Crea un array de colores y utiliza el
+// método shift() para eliminar el primer elemento del array. Imprima el array
+// actualizado.
+
+// let ColorsList = ["Rojo","Amarillo","Verde","Azul"]
+
+// console.log(ColorsList)
+
+// ColorsList.shift()
+
+// console.log(ColorsList)
+
+
+// Actividad 3 (Acceder a un elemento Específico): Crea un array que contenga
+// diferentes nombres de frutas y accede a un elemento específico utilizando su
+// índice (por ejemplo, el segundo elemento). Imprima el resultado.
+
+
+// let Fruits = ["Manzana","Pera","Fresa","Piña"]
+
+// let indicePera = Fruits.indexOf("Pera")
+
+// console.log(indicePera) // Imprime el número de indice
+// console.log(Fruits[1]) // Imprime la fruta en el número de indice #1
+
+
+// Actividad 4 (Eliminar un Elemento Específico por Índice): Crea un array con
+// números y elimina un elemento en una posición específica utilizando splice().
+// Luego, imprime el array resultante.
+
+// let Numbers = [1,4,3,5,7,8]
+
+// Numbers.splice(2,1) //Elimina el número 3 que esta en el indice 2
+
+// console.log(Numbers)
+
+
+// Actividad ( Agregar un elemento en un Índice Específico): Crea un array con
+// nombres de estudiantes y agrega un nuevo nombre en una posición específica
+// utilizando splice(). Finalmente, imprime el array actualizado.
+
+// let names = ["Maria","Juan","Pedro","Pablo"]
+
+// names.splice(2,0,"Juana") //Como no se elimina ningún el elmento se pone el 0 y luego el elemento que se quiere agregar "Juana"
+
+
+// console.log(names)
+
